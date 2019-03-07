6c05e9c8650da663ae70cb3b77893b52206c5c1e
Matheus Picioli <matheus.picioli98@gmail.com>
2019-03-07 11:56:17 -0300

    Form e validator instalados

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,11 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Produto;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController extends Controller
+class HelloController extends AbstractController
 {
 	/**
 	 * @Route("hello_world")
