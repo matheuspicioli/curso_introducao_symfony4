f971b56151f83328bcc41cd7683b94dc95edb820
Matheus Picioli <matheus.picioli98@gmail.com>
2019-03-07 11:42:47 -0300

    'Twig'

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Produto;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends Controller
+{
+	/**
+	 * @Route("hello_world")
+	 */
+	public function world()
+	{
+		return new Response(
+			"<html><body><h1>HELLO WORLD!</h1></body></html>"
+		);
+	}
+
+	/**
+	 * @Route("mostrar-mensagem")
+	 */
+	public function mensagem()
+	{
+		return $this->render("hello/mensagem.html.twig");
+	}
+
+	/**
+	 * @Route("cadastrar-produto")
+	 */
+	public function produto()
+	{
+		$em = $this->getDoctrine()->getManager();
+
+		$produto = new Produto;
+		$produto->setNome("Notebook")
+			->setPreco(1800);
+
+		$em->persist($produto);
+		$em->flush();
+
+		return new Response('O produto ' . $produto->getId() . ' foi criado');
+	}
+}
