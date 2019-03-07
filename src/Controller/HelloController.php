4bdb23499b8f68dfa3ed612bf8c5e070243d4797
Matheus Picioli <matheus.picioli98@gmail.com>
2019-03-07 14:06:16 -0300

    Hello controller/view para entender o fluxo com twig

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Produto;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -42,5 +45,31 @@ class HelloController extends AbstractController
 		$em->flush();
 
 		return new Response('O produto ' . $produto->getId() . ' foi criado');
+	}
+
+	/**
+	 * @Route("formulario")
+	 * @param Request $request
+	 * @return Response
+	 */
+	public function formulario(Request $request)
+	{
+		$produto 	= new Produto;
+
+		$form = $this->createFormBuilder($produto)
+				->add('nome', TextType::class)
+				->add('preco', TextType::class)
+				->add('enviar', SubmitType::class, ['label' => 'Salvar'])
+				->getForm();
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			return new Response('Formulário válido');
+		}
+
+		return $this->render('hello/formulario.html.twig', [
+			'form' => $form->createView()
+		]);
 	}
 }
